51c1ec80304cd25a7adcdbd316bcfff8e0fbbb43
Kodai Kabasawa <kabaaa1126@gmail.com>
2022-04-27 00:09:45 +0900

    add install command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Error, Write};
 use std::str;
 extern crate dirs;
 
-use clap::Command;
+use clap::{Arg, Command};
 use regex::Regex;
 
 fn setup() {
     let buf = [0; 1000];
-    let dir = std::env::current_dir().unwrap();
-    println!("{}", dir.to_str().unwrap());
     let home = dirs::home_dir().unwrap();
     let mut prof_path = home.clone();
     prof_path.push(".profile");
@@ -27,7 +25,52 @@ fn setup() {
     println!("{}", prof_content);
 }
 
-fn main() {
+async fn install(version: &str) {
+    let reg = Regex::new(r"(^\d+\.\d+\.\d+$)").unwrap();
+    if !reg.is_match(version) {
+        panic!("Unexpected version format provided: {}", version);
+    }
+    let home = dirs::home_dir().unwrap();
+    let phphp_path = home.join(".phphp");
+    // if Path::new(phphp_path.as_path()).is_dir() {
+    //     panic!(
+    //         "It looks like {} may already be installed, attempt clean up?",
+    //         version
+    //     );
+    // }
+    println!("Downloading {}", version);
+    let url = format!("https://www.php.net/distributions/php-{}.tar.gz", version);
+    let response = reqwest::get(url)
+        .await
+        .unwrap_or_else(|_| panic!("Could not find {} via php.net releases", version));
+    let content = response.text().await.unwrap();
+    let tmp_dir = phphp_path.join("tmp");
+    let install_dir = phphp_path.join("lib");
+    let _ = std::fs::create_dir(install_dir.as_path());
+    let tmp_path = tmp_dir.join(format!("php-{}.tar.gz", version));
+    let _ = std::fs::create_dir_all(tmp_dir);
+    // let _ = copy(
+    //     &mut content.as_bytes(),
+    //     &mut File::create(tmp_path).unwrap(),
+    // );
+    println!("tmp_path, {}", &tmp_path.to_str().unwrap());
+    let _ = std::fs::File::create(&tmp_path)
+        .unwrap()
+        .write_all(content.as_bytes());
+    let _ = std::process::Command::new("tar")
+        .args([
+            "-xzf",
+            tmp_path.to_str().unwrap(),
+            "--strip-components=1 -C",
+            install_dir.to_str().unwrap(),
+        ])
+        .output()
+        .expect("failed to handle tar");
+    let _ = std::fs::remove_file(&tmp_path).expect("failed to remove tmp file");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let matches = Command::new("phphp")
         .about("php version manager")
         .version("0.1.0")
@@ -38,7 +81,11 @@ fn main() {
         .subcommand(Command::new("list"))
         .subcommand(Command::new("use"))
         .subcommand(Command::new("clean"))
-        .subcommand(Command::new("install"))
+        .subcommand(
+            Command::new("install")
+                .arg_required_else_help(true)
+                .arg(Arg::new("version").takes_value(true)),
+        )
         .subcommand(Command::new("recompile"))
         .subcommand(Command::new("uninstall"))
         .subcommand(Command::new("install-dependencies"))
@@ -47,8 +94,13 @@ fn main() {
         Some(("setup", _)) => {
             setup();
         }
+        Some(("install", arg)) => {
+            let version = arg.value_of("version").unwrap();
+            install(version).await;
+        }
         _ => {
             unimplemented!();
         }
     }
+    Ok(())
 }
